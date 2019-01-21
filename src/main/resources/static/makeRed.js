6a612a38b23b458ace5a265e44278a5961d6f0e4
michael.sampson <michael.sampson@kobie.com>
2019-01-21 07:15:26 -0500

    improvements

--- src/main/resources/static/makeRed.js
+++ src/main/resources/static/makeRed.js
@@ -6,7 +6,23 @@ function turnToRed(){
     {
         var first = thewords[i];
         var lastLetter = first.charAt(first.length -1);
-        document.write( ' ' + first.substring(0, first.length - 1 ) + lastLetter.fontcolor("red").bold( )  );
-
+        var hadpunctuation = ""
+        if(lastLetter == "." || lastLetter=="," || lastLetter=="!") {
+            hadpunctuation = first.charAt(first.length - 2).fontcolor("red").bold() + first.charAt(first.length -1);
+            first = first.substring(0, first.length -1)
+            lastLetter="";
+        }
+        if(first.length > 1)
+            var theText = ' ' + first.substring(0,1).fontcolor("green") + first.substring(1, first.length - 1 )
+                + lastLetter.fontcolor("red").bold( ) + hadpunctuation;
+        else
+            var theText = ' ' + first.fontcolor("red")
+        theText = theText.fontsize(12);
+        var writereturned =  document.write(  theText );
+        var elementfound = document.getElementById("thecontent");
+        hadpunctuation="";
+        lastLetter="";
     }
+    document.body.style.textAlign = "center";
+    document.body.style.width="900px"
 }
